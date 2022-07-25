8073d6dd020610508b04ad76b9e1531585056fbb
Cosmin Ene <enecosminmihai@outlook.com>
2022-07-25 19:37:41 +0300

    Serialize flask models with marshmallow for api json display

--- main/app.py
+++ main/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -13,6 +14,8 @@ CORS(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+ma = Marshmallow(app)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
@@ -26,9 +29,19 @@ class ProductUser(db.Model):
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
-@app.route('/')
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+class ProductUserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ProductUser
+
+@app.route('/api/products')
 def index():
-    return "Hello World"
+    products = Product.query.all()
+    products_schema = ProductSchema(many=True)
+    return jsonify(products_schema.dump(products))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
